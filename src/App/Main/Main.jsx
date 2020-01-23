61280f4ca801fe19b7ffc7b64bc073f0d44df45c
Nathdanai Somprasong <tam@Nathdanais-MacBook-Pro.local>
2020-01-22 17:54:04 -0800

    change

--- src/App/Main/Main.jsx
+++ src/App/Main/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
 
 <div class="example">
     
-    <p>Image</p>
+    <p>Banner Image</p>
 
 </div>
 
@@ -15,15 +15,40 @@ const Main = () => {
 
 
 <ul class="Tour">
-  <li>  <h2>Tour 1</h2> 
-    <p>Descriptions</p> </li>
+  <li>  <h2>Tour 1</h2>  
+    <p>Descriptions</p> <p>Price</p><p>See more</p> </li>
   <li>  <h2>Tour 2</h2>
-    <p>Descriptions</p> </li>
+    <p>Descriptions</p> <p>Price</p><p>See more</p></li>
   <li>  <h2>Tour 3</h2>
-    <p>Descriptions</p></li>
+    <p>Descriptions</p> <p>Price</p><p>See more</p></li>
   <li>  <h2>Tour 4</h2>
-    <p>Descriptions</p> </li>
+    <p>Descriptions</p> <p>Price</p><p>See more</p></li>
 </ul>
+
+
+
+
+
+<ul class="Blog">
+  <li>  <h2>Blog1</h2>  <h3>Blog 1</h3>
+    <p>Descriptions</p> <p>Read more</p> </li>
+
+  <li>  <h2>Blog2</h2> <h3>Blog 2</h3>
+    <p>Descriptions</p> <p>Read</p><p>See more</p></li>
+
+  <li>  <h2>Blog 3</h2> <h3>Blog 3</h3>
+    <p>Descriptions</p> <p>Read</p><p>See more</p></li>
+
+  <li>  <h2>Blog 4</h2> <h3>Blog 4</h3>
+    <p>Descriptions</p> <p>Read</p><p>See more</p></li>
+</ul>
+
+
+
+
+
+
+
 
 
 
@@ -34,6 +59,20 @@ const Main = () => {
 </main>
     )
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
     
 };
