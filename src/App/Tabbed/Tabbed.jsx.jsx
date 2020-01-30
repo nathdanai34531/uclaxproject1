09bd4181f9e2b56a8cf1d7f6842ebb059e32df9e
Nathdanai Somprasong <tam@Nathdanais-MacBook-Pro.local>
2020-01-29 18:25:47 -0800

    Add Tabbed

--- src/App/Tabbed/Tabbed.jsx.jsx
+++ src/App/Tabbed/Tabbed.jsx.jsx
@@ -1,52 +1,58 @@
 import React, { useState } from 'react';
 import './Tabbed.scss';
 
+
+//Arrays
 const tabItems = [
     {
-        title: 'Gallery1',
-        text: 'The first gallery',
+  
+        title: 'Gallery 1',
+        text: 'First gallery',
+
     },
 
     {
-        title: 'Gallery2',
-        text: 'The second gallery',
+ 
+        title: 'Gallery 2',
+        text: 'Second gallery',
+
     },
 
     {
-        title: 'Gallery3',
-        text: 'The third gallery',
+
+        title: 'Gallery 3',
+        text: 'Third gallery',
+
     },
 
 ];
 
+
+
 const Tabbed = () => {
-    //Stage managed
-    const [currentTab, setCurrentTab] = useState(tabItems[0]);
+    //stage managed
+    const [currentTab, setCurrentTab] = useState(tabItems[1]);
 
 
     return (
         <div className={'Tabbed'}>
-            
-            <div className="tabs">
-                <div onCLick={ () => { setCurrentTab (tabItems[0] )} } className="tab">Gallery 1</div>
-                <div onCLick={ () => { setCurrentTab (tabItems[1] )} } className="tab">Gallery 2</div>
-                <div onCLick={ () => { setCurrentTab (tabItems[2] )} } className="tab">Gallery 3</div>
-            </div>
 
+            <div className="tabs">
+
+
+                <div onClick={ () => { setCurrentTab(tabItems[0]) } } className="tab">Gallery 1</div>
+                <div onClick={ () => { setCurrentTab(tabItems[1]) } }className="tab">Gallery 2</div>
+                <div onClick={ () => { setCurrentTab(tabItems[2]) } }className="tab">Gallery 3</div>
+            </div>
 
             <div className="content">
-            
+                <img src={currentTab.image} />
                 <h2>{currentTab.title}</h2>
                 <p>{currentTab.text}</p>
-
             </div>
-
         </div>
-
-
     )
 
 }
 
 export default Tabbed;
-
