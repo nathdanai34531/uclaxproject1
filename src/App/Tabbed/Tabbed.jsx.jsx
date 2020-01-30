97eb5fa7cdda417d85a8f70fa44c3318cd235c6a
Nathdanai Somprasong <tam@Nathdanais-MacBook-Pro.local>
2020-01-29 18:11:13 -0800

    Add tabbed

--- src/App/Tabbed/Tabbed.jsx.jsx
+++ src/App/Tabbed/Tabbed.jsx.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Tabbed.scss';
 
+const tabItems = [
+    {
+        title: 'Gallery1',
+        text: 'The first gallery',
+    },
+
+    {
+        title: 'Gallery2',
+        text: 'The second gallery',
+    },
+
+    {
+        title: 'Gallery3',
+        text: 'The third gallery',
+    },
+
+];
+
 const Tabbed = () => {
+    //Stage managed
+    const [currentTab, setCurrentTab] = useState(tabItems[0]);
+
+
     return (
-        <div className={"Tabbed"}>
+        <div className={'Tabbed'}>
+            
             <div className="tabs">
-                <div className="tab">Gallery 1</div>
-                <div className="tab">Gallery 2</div>
-                <div className="tab">Gallery 3</div>
+                <div onCLick={ () => { setCurrentTab (tabItems[0] )} } className="tab">Gallery 1</div>
+                <div onCLick={ () => { setCurrentTab (tabItems[1] )} } className="tab">Gallery 2</div>
+                <div onCLick={ () => { setCurrentTab (tabItems[2] )} } className="tab">Gallery 3</div>
             </div>
-       
 
-        <div className="content">
 
-            <h2>Gallery 1</h2>
-            <p></p>
-        </div>
+            <div className="content">
+            
+                <h2>{currentTab.title}</h2>
+                <p>{currentTab.text}</p>
+
+            </div>
 
         </div>
 
