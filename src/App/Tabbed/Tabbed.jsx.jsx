6d4cd47e964c643fc2e1e94d6917dc8cf0e9b9dd
Nathdanai Somprasong <tam@Nathdanais-MacBook-Pro.local>
2020-01-30 17:35:47 -0800

    Update tabbed

--- src/App/Tabbed/Tabbed.jsx.jsx
+++ src/App/Tabbed/Tabbed.jsx.jsx
@@ -31,7 +31,11 @@ const tabItems = [
 
 const Tabbed = () => {
     //stage managed
-    const [currentTab, setCurrentTab] = useState(tabItems[1]);
+    const [currentTab, setCurrentTab] = useState(tabItems[0]);
+
+    const isG1 = (currentTab.title === 'Gallery 1') ? 'tab active':'tab';
+    const isG2 = (currentTab.title === 'Gallery 2') ? 'tab active':'tab';
+    const isG3 = (currentTab.title === 'Gallery 3') ? 'tab active':'tab';
 
 
     return (
@@ -40,9 +44,9 @@ const Tabbed = () => {
             <div className="tabs">
 
 
-                <div onClick={ () => { setCurrentTab(tabItems[0]) } } className="tab">Gallery 1</div>
-                <div onClick={ () => { setCurrentTab(tabItems[1]) } }className="tab">Gallery 2</div>
-                <div onClick={ () => { setCurrentTab(tabItems[2]) } }className="tab">Gallery 3</div>
+                <div onClick={ () => { setCurrentTab(tabItems[0]) } }className={isG1}>Gallery 1</div>
+                <div onClick={ () => { setCurrentTab(tabItems[1]) } }className={isG2}>Gallery 2</div>
+                <div onClick={ () => { setCurrentTab(tabItems[2]) } }className={isG3}>Gallery 3</div>
             </div>
 
             <div className="content">
